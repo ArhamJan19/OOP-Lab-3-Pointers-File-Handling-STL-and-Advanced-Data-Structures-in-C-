320a62c45a1f916550eb1a6654adcf922f8255b2
ArhamJan19 <146324130+ArhamJan19@users.noreply.github.com>
2024-03-05 01:48:30 +0500

    Task 1 till Task 4 Completed

--- StudentManager.h
+++ StudentManager.h
@@ -1,26 +1,26 @@
-//
-//#pragma once
-//
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//
-//class StudentManager {
-//private:
-//    struct Student {
-//        int id;
-//        std::string name; 
-//        float grade;
-//
-//        Student(int i, const std::string& n, float g) : id(i), name(n), grade(g) {}
-//    };
-//
-//    std::vector<Student> records; 
-//
-//public:
-//    void addStudent(int id, const std::string& name, float grade);
-//    void updateGrade(int id, float newGrade);
-//    void displayRecords() const;
-//
-//    ~StudentManager();
-//};
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class StudentManager {
+private:
+    struct Student {
+        int id;
+        std::string name; 
+        float grade;
+
+        Student(int i, const std::string& n, float g) : id(i), name(n), grade(g) {}
+    };
+
+    std::vector<Student> records; 
+
+public:
+    void addStudent(int id, const std::string& name, float grade);
+    void updateGrade(int id, float newGrade);
+    void displayRecords() const;
+
+    ~StudentManager();
+};
--- main (StudentManager).cpp
+++ main (StudentManager).cpp
@@ -1,15 +1,15 @@
-//
-//#include "StudentManager.h"
-//#include <iostream>    
-//
-//int main() {
-//	StudentManager sm;
-//	sm.addStudent(1, "John Doe", 92.5);
-//	sm.addStudent(2, "Jane Doe", 88.0);
-//	sm.displayRecords();
-//	sm.updateGrade(1, 95.0);
-//	std::cout << "After grade update:" << std::endl;
-//	sm.displayRecords();
-//
-//	return 0;
-//}
+
+#include "StudentManager.h"
+#include <iostream>    
+
+int main() {
+	StudentManager sm;
+	sm.addStudent(1, "John Doe", 92.5);
+	sm.addStudent(2, "Jane Doe", 88.0);
+	sm.displayRecords();
+	sm.updateGrade(1, 95.0);
+	std::cout << "After grade update:" << std::endl;
+	sm.displayRecords();
+
+	return 0;
+}
